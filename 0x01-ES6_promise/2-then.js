088d8b1ae65f505826eb74e7a299caffb4d0be85
David-Inkheart <okoliedave1@gmail.com>
2023-04-19 11:12:18 +0100

    changed capital S to s

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 // This function has three handlers: one for success,
-// one for failure, and a log. The log is always called for revery "resolution" (resolve).
+// one for failure, and a log.
 
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'Success' };
+      return { status: 200, body: 'success' };
     })
     .catch(() => new Error());
 }
