a290a0b374d6bf92e743fc07fc4dcbae07ad8fb5
David-Inkheart <okoliedave1@gmail.com>
2023-04-19 15:23:35 +0100

    async/await test

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,12 @@
 // Import uploadPhoto and createUser from utils.js
 // Write an async function named asyncUploadUser that will call these two functions
 // and return an object with the following format
+// If one of the async function fails, return an empty object. Example:
 
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   const photo = await uploadPhoto();
   const user = await createUser();
-  if (photo && user) {
-    return { photo, user };
-  }
-  return { photo: null, user: null };
+  return { photo, user };
 }
