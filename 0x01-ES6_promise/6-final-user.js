196d370c91b156f5a5e667717dc07b519dd61905
David-Inkheart <okoliedave1@gmail.com>
2023-04-19 13:11:00 +0100

    handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,11 +6,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  let arr = [];
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
-      results.forEach((result) => ({
+      results.forEach((result) => {
+        arr.push({
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : String(result.reason),
-      }));
+        });
+      });
+        console.log(arr);
+        return arr;
     });
 }
