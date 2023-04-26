817f938a4a8f839ecba5cbc3972ebc6d1bdb9523
David-Inkheart <okoliedave1@gmail.com>
2023-04-26 18:18:39 +0100

    function interface

--- 0x04-TypeScript/README.md
+++ 0x04-TypeScript/README.md
@@ -3,7 +3,73 @@
 # TASKS
 
 ### [0. Creating an interface for a student](./task_0/js/main.ts)
+Write your code in the `main.ts` file:
 
+- Write an interface named Student that accepts the following elements: `firstName(string)`, `lastName(string)`, `age(number)`, and `location(string)`
+- Create two students, and create an array named `studentsList` containing the two variables
+- Using Vanilla Javascript, render a table and for each elements in the array, append a new row to the table
+- Each row should contain the first name of the student and the location
 
 ### [1. Let's build a Teacher interface](./task_1/js/main.ts)
+
+- `firstName(string)` and `lastName(string)`. These two attributes should only be modifiable when a Teacher is first initialized
+- `fullTimeEmployee(boolean)` this attribute should always be defined
+- `yearsOfExperience(number)` this attribute is optional
+- `location(string)` this attribute should always be defined
+- Add the possibility to add any attribute to the Object like `contract(boolean)` without specifying the name of the attribute
+
+Example: 
+```
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+```
+should print
+```
+{
+    contract: false
+    firstName: "John"
+    fullTimeEmployee: false
+    lastName: "Doe"
+}
+```
+
+### [2. Extending the Teacher class](./task_1/js/main.ts)
+
+- Write an interface named `Directors` that [extends](https://www.typescriptlang.org/docs/handbook/interfaces.html#introduction) `Teacher`. It requires an attribute named `numberOfReports(number)`
+
+Example:
+```
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+```
+should print
+```
+{
+    firstName: "John"
+    fullTimeEmployee: true
+    lastName: "Doe"
+    location: "London"
+    numberOfReports: 17
+}
+```
+
+### [3. Printing teachers](./task_1/js/main.ts)
+Write a function `printTeacher`:
+- It accepts two arguments `firstName` and `lastName`
+- It returns the first letter of the firstName and the full lastName
+- Example: `printTeacher("John", "Doe") -> J. Doe`
+Write an [interface for the function](https://www.typescriptlang.org/docs/handbook/interfaces.html#function-types) named `printTeacherFunction`.
 
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,5 +1,15 @@
 // Create a directory task_1 and copy these configuration files into this folder: package.json, tsconfig.json, webpack.config.js
 
+// The documentation that helped me solve each task is listed above each task as a comment called "Sources"
+
+// testing any task cab be done as a .js file and run with node
+// example: node main.js
+// .ts can be compiled locally into a .js file using the command `tsc main.ts`. This will create a main.js
+// file in the same directory as the main.ts file. tsc is the TypeScript compiler. It takes in TypeScript
+// code and spits out JavaScript code. run `npm install -g typescript` on the terminal before using tsc.
+
+
+// TASK 1
 // firstName(string) and lastName(string). These two attributes should only be modifiable when a Teacher is first initialized
 // fullTimeEmployee(boolean) this attribute should always be defined
 // yearsOfExperience(number) this attribute is optional
@@ -8,9 +18,6 @@
 
 // Sources: https://www.typescriptlang.org/docs/handbook/interfaces.html
 
-// .ts can be compiles locally into a .js file using the command tsc main.ts. This will create a main.js
-// file in the same directory as the main.ts file. tsc is the TypeScript compiler. It takes in TypeScript
-// code and spits out JavaScript code. `npm install -g typescript` before using.
 
 interface Teacher {
     readonly firstName: string;
@@ -31,3 +38,42 @@ interface Teacher {
 // };
 
 // console.log(teacher3);
+
+// TASK 2
+// Write an interface named Directors that extends Teacher. It requires an attribute named numberOfReports(number)
+
+// source: https://www.typescriptlang.org/docs/handbook/interfaces.html#extending-interfaces
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+// test as a .js file
+// const director1: Directors = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   location: 'London',
+//   fullTimeEmployee: true,
+//   numberOfReports: 17,
+// };
+// console.log(director1);
+
+
+// TASK 3
+// Write a function named `printTeacher:` It accepts two arguments: `firstName` and `lastName`
+// it returns the first letter of the firstName and the full lastName: `printTeacher("John", "Doe")` -> "J. Doe"
+// Write an interface for the function named printTeacherFunction.
+
+// source: https://www.typescriptlang.org/docs/handbook/interfaces.html#function-types
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+let printTeacher: printTeacherFunction;
+
+printTeacher = (firstName: string, lastName: string): string => `${firstName.charAt(0)}. ${lastName}`
+
+// test as a .js file
+// console.log(printTeacher('John', 'Doe'));
+
