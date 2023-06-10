87d9fdcbfcff50391bb7c5fbdb94cd8070f02861
David-Inkheart <okoliedave1@gmail.com>
2023-06-10 18:56:59 +0100

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,7 +1,23 @@
 const fs = require('fs/promises');
 
 async function countStudents(path) {
-    
+  if (!path) {
+    throw new Error('Cannot load the database');
+  }
+  try {
+    const data = await fs.readFile(path, 'utf-8'); // read file asynchronously
+    const lines = data.split('\n').filter((line) => line !== '');
+    const columns = lines.shift().split(',');
+    const students = lines.map((line) => line.split(','));
+    const fieldIndex = columns.indexOf('field');
+    const CSstudents = students.filter((student) => student[fieldIndex] === 'CS').map((student) => student[0]);
+    const SWEstudents = students.filter((student) => student[fieldIndex] === 'SWE').map((student) => student[0]);
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${CSstudents.length}. List: ${CSstudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${SWEstudents.length}. List: ${SWEstudents.join(', ')}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
