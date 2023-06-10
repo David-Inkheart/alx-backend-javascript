d921c2059274832a19a9a79c6d344dd916964a23
David-Inkheart <okoliedave1@gmail.com>
2023-06-10 15:30:52 +0100

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,31 +1,26 @@
 const fs = require('fs');
 
-function countStudents (path) {
-  // attempt to read the database file synchronously
-  try {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        throw new Error('Cannot load the database');
-      }
-      // console.log(data);
-      const lines = data.split('\n').filter((line) => line !== '');
-      // console.log(lines);
-      const columns = lines.shift().split(',');
-      // console.log(columns);
-      const students = lines.map((line) => line.split(','));
-      // console.log(students);
-      const fieldIndex = columns.indexOf('field');
-      // console.log(fieldIndex);
-      const CSstudents = students.filter((student) => student[fieldIndex] === 'CS').map((student) => student[0]);
-      // console.log(CSstudents);
-      const SWEstudents = students.filter((student) => student[fieldIndex] === 'SWE').map((student) => student[0]);
-      console.log(`Number of students: ${students.length}`);
-      console.log(`Number of students in CS: ${CSstudents.length}. List: ${CSstudents.join(', ')}`);
-      console.log(`Number of students in SWE: ${SWEstudents.length}. List: ${SWEstudents.join(', ')}`);
-    });
-  } catch (err) {
-    throw err;
-  }
+function countStudents(path) {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    // console.log(data);
+    const lines = data.split('\n').filter((line) => line !== '');
+    // console.log(lines);
+    const columns = lines.shift().split(',');
+    // console.log(columns);
+    const students = lines.map((line) => line.split(','));
+    // console.log(students);
+    const fieldIndex = columns.indexOf('field');
+    // console.log(fieldIndex);
+    const CSstudents = students.filter((student) => student[fieldIndex] === 'CS').map((student) => student[0]);
+    // console.log(CSstudents);
+    const SWEstudents = students.filter((student) => student[fieldIndex] === 'SWE').map((student) => student[0]);
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${CSstudents.length}. List: ${CSstudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${SWEstudents.length}. List: ${SWEstudents.join(', ')}`);
+  });
 }
 
 module.exports = countStudents;
