b4de0b683801df3979cdfcc1ee5b7c73516890ed
David-Inkheart <okoliedave1@gmail.com>
2023-06-14 18:33:16 +0100

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,23 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+var assert = require('assert');
+var calculateNumber = require('./0-calcul')
 
 describe('calculateNumber', function () {
-  it('should return the sum of two rounded numbers', function () {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it('should return 4 when adding 1 and 3', function () {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('should return 5 when adding 1 and 3.7', function () {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it('should return 5 when adding 1.2 and 3.7', function () {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should return 6 when adding 1.5 and 3.7', function () {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it('should return 0 when adding 0.1 and 0.3', function () {
+    assert.equal(calculateNumber(0.1, 0.3), 0);
+  });
+  it('should return -4 when adding -1.1 and -2.7', function () {
+    assert.equal(calculateNumber(-1.1, -2.7), -4);
   });
 });
